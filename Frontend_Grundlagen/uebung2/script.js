e84b3a444d3a8054271ddd01244de330fdab9132
kevinforter <49223606+kevinforter@users.noreply.github.com>
2023-10-29 01:09:46 +0200

    add dynamic and events

---
+++ Frontend_Grundlagen/uebung2/script.js
@@ -0,0 +1,11 @@
+function onClick() {
+    alert("Mitteilung wurde gesendet");
+}
+
+function onMouseMove() {
+    alert("Hier kann man etwas eingeben");
+}
+
+function onPagesShow() {
+    alert("Hello World");
+}
