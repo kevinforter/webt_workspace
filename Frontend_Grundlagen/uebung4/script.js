262f46a1a0ee78db68e094ec126420e1e51fbe5f
kevinforter <49223606+kevinforter@users.noreply.github.com>
2023-10-30 00:43:15 +0100

    add script and css

--- Frontend_Grundlagen/uebung4/script.js
+++ Frontend_Grundlagen/uebung4/script.js
@@ -2,5 +2,10 @@ function showRandomNumber(min, max) {
     let x = (Math.random() * (max - min)) + min;
 
     // TODO: complete code according to task description
+    let randomNr = document.getElementById("zufallszahl");
+    randomNr.innerText = x.toFixed(0);
 
+    if (x < 50) {
+        randomNr.classList.add('green');
+    }
 }
